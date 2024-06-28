6528491e2c9846c502e593ac9dbebabea292573d
Lars Vonk <larserikvonk@ziggo.nl>
2024-06-28 18:35:01 +0200

    chore: 2023 day 13

--- 2015/13/index.js
+++ 2015/13/index.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
-const dataStr = fs.readFileSync('input.json', 'utf8');
-let data = JSON.parse(dataStr);
+const dataStr = fs.readFileSync('input.txt', 'utf8');
+let data = dataStr.split('\n').filter(Boolean);
 
 data = data.map(line => {
 	const words = line.split(' ');
@@ -17,10 +17,126 @@ data = data.map(line => {
 	};
 });
 
-let maxHapiness = 0;
+const people = data.reduce((acc, { startingPerson, endingPerson }) => {
+	if (!acc.includes(startingPerson)) {
+		acc.push(startingPerson);
+	}
 
-function getPossibleArrangements(people) {
-	let arrangements = [];
+	if (!acc.includes(endingPerson)) {
+		acc.push(endingPerson);
+	}
 
-	const uniquePeople = new Set(people.map(person => person.startingPerson));
+	return acc;
+}, []);
+
+function permute(arr) {
+	// Early return if only 1 or less is left with array of that value
+	if (arr.length <= 1) {
+		return [arr];
+	}
+
+	const permutations = [];
+
+	// Loop through all
+	for (let i = 0; i < arr.length; i++) {
+		// Get current person
+		const currentPerson = arr[i];
+
+		// Get all elements before and after current person
+		const remainingPeople = arr.slice(0, i).concat(arr.slice(i + 1));
+		const remainingPermutations = permute(remainingPeople);
+
+		// Loop over all possible permutations
+		for (let perm of remainingPermutations) {
+			// Add current person to all permutations
+			permutations.push([currentPerson, ...perm]);
+		}
+	}
+
+	return permutations;
 }
+
+function circularPermutations(people) {
+	// Early return if array is empty
+	if (people.length === 0) {
+		return [];
+	}
+
+	// Get first person
+	const firstPerson = people[0];
+	// Get all people except first person
+	const remainingPeople = people.slice(1);
+	// Get all combinations of remaining people
+	const remainingPeopleCombinations = permute(remainingPeople);
+
+	// Initialise result array
+	const circularPerms = [];
+	for (let perm of remainingPeopleCombinations) {
+		// Add element with first person and all different permutations of remaining people
+		circularPerms.push([firstPerson, ...perm]);
+	}
+
+	return circularPerms;
+}
+
+function mapHappiness(result) {
+	// Initialise happiness to 0
+	let happiness = 0;
+
+	for (let i = 0; i < result.length; i++) {
+		// Get first person
+		const currentPerson = result[i];
+		// Get person next to first person
+		const nextPerson = result[(i + 1) % result.length];
+
+		// Get data object from first person
+		const currentHappiness = data.find(({ startingPerson, endingPerson }) => {
+			return startingPerson === currentPerson && endingPerson === nextPerson;
+		});
+		// Get data object from next person next to first person
+		const nextHappiness = data.find(({ startingPerson, endingPerson }) => {
+			return startingPerson === nextPerson && endingPerson === currentPerson;
+		});
+
+		// Add happiness from both objects (can be negative if people dont like each other)
+		happiness += currentHappiness.happiness;
+		happiness += nextHappiness.happiness;
+	}
+
+	return {
+		arrangement: result,
+		happiness,
+	};
+}
+
+const permutations = circularPermutations(people).map(mapHappiness);
+
+const maxHappiness = permutations.reduce((acc, { happiness }) => {
+	return Math.max(acc, happiness);
+}, -Infinity);
+
+console.log(`Part 1: ${maxHappiness}`);
+
+const newPeople = [...people, 'me'];
+
+for (let person of people) {
+	data.push({
+		startingPerson: 'me',
+		endingPerson: person,
+		happiness: 0,
+	});
+
+	data.push({
+		startingPerson: person,
+		endingPerson: 'me',
+		happiness: 0,
+	});
+}
+
+const newPermutations = circularPermutations(newPeople).map(mapHappiness);
+
+const newMaxHappiness = newPermutations.reduce((acc, { happiness }) => {
+	return Math.max(acc, happiness);
+}, -Infinity);
+
+console.log(`Part 2: ${newMaxHappiness}`);
